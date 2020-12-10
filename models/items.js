1141f02fe218faee13ad2a4715513d5ea0410975
ovokpus <ovo@live.ca>
2020-12-10 05:19:51 +0100

    added database and model files

---
+++ models/items.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+let itemSchema = new Schema({
+    description: { type: String, required: true },
+    date: { type: Date, default: Date.now }
+});
+
+var Item = mongoose.model('Item', itemSchema);
+
+module.exports = Item;
